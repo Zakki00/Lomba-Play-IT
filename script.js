082f546476efc80f1eca24182af65cab18867c61
Zakki00 <zakkimubarroq06@gmail.com>
2024-10-16 02:24:09 +0700

    menambahkan konten baru di folder budaya

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-const menuIcon = document.getElementById("menu-icon");
-const menuList = document.getElementById("menu-list");
-const menuItems = menuList.querySelectorAll("li");
 
-menuIcon.addEventListener("click", () => {
-    menuList.classList.toggle("active");
-    if (menuList.classList.contains("active")) {
-        menuItems.forEach((item, index) => {
-            item.style.transitionDelay = `${index * 0.1}s`;
-            item.style.opacity = '1';
-            item.style.transform = 'translateY(0)';
-        });
-    } else {
-        menuItems.forEach(item => {
-            item.style.opacity = '0';
-            item.style.transform = 'translateY(-10px)';
-        });
-    }
+function toggleExpand(element) {
+    element.classList.toggle('expanded');
+}
+ 
+// script.js
+document.querySelector('.toggle').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
 });
+    
